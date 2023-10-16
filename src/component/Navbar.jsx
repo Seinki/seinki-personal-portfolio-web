ae20d6b4b338074fa042983625e176cadc80c985
Seinki <121332397+Seinki@users.noreply.github.com>
2023-10-16 22:56:46 +0700

    styling navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,29 +4,34 @@ export default function Navbar() {
   const logo = personalData.main.logo;
 
   return(
-    <nav>
-      <div className="nav-logo">
-        <a href="#">
-          <img src={logo} alt="logo" />
-        </a>
-      </div>
-      <ul className="nav-links">
-        <li className="link">
-          <a href="#">About</a>
-        </li>
-        <li className="link">
-          <a href="#">Resume</a>
-        </li>
-        <li className="link">
-          <a href="#">Skills</a>
-        </li>
-        <li className="link">
-          <a href="#">Portfolio</a>
-        </li>
-        <li className="link">
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <div className="container-navbar">
+      <nav className='navbar'>
+        <div className="nav-logo">
+          <a href="#">
+            <img src={logo} alt="logo" />
+          </a>
+        </div>
+        <ul className="nav-links">
+          <li className="link">
+            <a href="#">About</a>
+          </li>
+          <li className="link">
+            <a href="#">Resume</a>
+          </li>
+          <li className="link">
+            <a href="#">Skills</a>
+          </li>
+          <li className="link">
+            <a href="#">Portfolio</a>
+          </li>
+          <li className="link">
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <button className="btn">
+          <a href="#">Hire Me</a>
+        </button>
+      </nav>
+    </div>
   )
 }
